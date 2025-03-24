b79839cd34f5c7a0bad6e9a658c6f197a7f48e51
muktiabdii <abdisyukur10@gmail.com>
2025-03-24 21:34:28 +0700

    Event handling thumbs up

--- app/src/main/java/com/example/kulubs/DeskripsiWarungActivity.kt
+++ app/src/main/java/com/example/kulubs/DeskripsiWarungActivity.kt
@@ -1,4 +1,25 @@
 package com.example.kulubs
 
-class DeskripsiWarungActivity {
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class DeskripsiWarungActivity : AppCompatActivity() {
+    private var isLiked = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deskripsi_warung) // Sesuaikan dengan layout yang benar
+
+        val thumbsUpButton = findViewById<ImageButton>(R.id.thumbsUpButton)
+
+        thumbsUpButton.setOnClickListener {
+            isLiked = !isLiked
+            if (isLiked) {
+                thumbsUpButton.setImageResource(R.drawable.ic_thumbs_up_2) // Ikon berwarna
+            } else {
+                thumbsUpButton.setImageResource(R.drawable.ic_thumbs_up) // Ikon stroke
+            }
+        }
+    }
 }
